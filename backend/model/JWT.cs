e5cba1f22d1e8da52305479755211f73fe9ef18d
PhThDat <ptdat01012003@gmail.com>
2022-11-16 15:01:55 +0700

    asda

--- backend/model/JWT.cs
+++ backend/model/JWT.cs
@@ -1,3 +1,24 @@
+using System.Security.Cryptography;
+using System.Text;
 
 namespace BackEndCSharp.Model;
 
+class JWT
+{
+    public string Header { get => header; }
+    public string Payload { get => payload; }
+    public string Signature { get => signture; }
+    private string header { get; set; }
+    private string payload { get; set; }
+    private string signture { get; set; }
+    public JWT(string _header, string _payload, string _sign)
+    {
+        byte[] buffer = System.Convert.FromBase64String(_header);
+        header = Encoding.UTF8.GetString(buffer);
+
+        buffer = System.Convert.FromBase64String(_payload);
+        payload = Encoding.UTF8.GetString(buffer);
+        signture = _sign;
+
+    }
+}
